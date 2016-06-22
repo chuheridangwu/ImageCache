b0392fea736997b08a218ad7ea578093e8bb83b2
董玉毛 <153100950@qq.com>
2016-06-22 20:24:08 +0800

    防止重复下载操作，使用字典，图片URL为key,下载对象为Value。首先冲字典中取出下载对象，如果对象不存在，创建下载对象，装进字典中

--- ImageCache/ViewController.m
+++ ImageCache/ViewController.m
@@ -17,6 +17,8 @@
 @property (nonatomic,strong)NSArray *modelArray;
 // 存放所有下载操作的队列
 @property (nonatomic,strong)NSOperationQueue *queue;
+// 存放所有下载操作(url是key,operation对象是value)
+@property (nonatomic,strong)NSMutableDictionary *operations;
 @end
 
 @implementation ViewController
@@ -42,6 +44,13 @@
     return _queue;
 }
 
+- (NSMutableDictionary*)operations{
+    if (!_operations) {
+        _operations = [NSMutableDictionary dictionary];
+    }
+    return _operations;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -62,25 +71,35 @@
     Model *model = self.modelArray[indexPath.row];
     cell.textLabel.text = model.name;
     
-    //创建操作，下载图片
-    NSBlockOperation *operation = [NSBlockOperation blockOperationWithBlock:^{
-        NSURL *url = [NSURL URLWithString:model.icon];
-        UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:url]];
-        //回到主线程
-        [[NSOperationQueue mainQueue]addOperationWithBlock:^{
-             cell.imageView.image = image;
+    //取出当前图片URL对应的下载操作(operation对象)
+    NSBlockOperation *operation = self.operations[model.icon];
+    if (!operation) {
+        //创建操作，下载图片
+        operation = [NSBlockOperation blockOperationWithBlock:^{
+            NSURL *url = [NSURL URLWithString:model.icon];
+            UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:url]];
+            //回到主线程
+            [[NSOperationQueue mainQueue]addOperationWithBlock:^{
+                cell.imageView.image = image;
+            }];
+            
         }];
-       
-    }];
+        
+        //添加操作到队列中
+        [self.queue addOperation:operation];
+        
+        //将下载操作添加到字典
+        self.operations[model.icon] = operation;
+    }
     
-    //添加操作到队列中
-    [self.queue addOperation:operation];
     return cell;
 }
 
 /*
  1.将下载图片的耗时操作放在子线程去做，有数据后返回主线程设置UI
- 2.如何防止重复下载操作
+ 2.如何防止重复下载操作，只需要保证一张图片只下载一次.
+   从字典中取出NSBlockOperation对象，如果对象存在，不需要创建。如果不存在，创建对象，将对象存进字典
+ 
  
  */
 
